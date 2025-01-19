60a14ce0167fbbfaaef514758ea61c4d32efbe13
Lukerator <lucadarius41@yahoo.com>
2025-01-19 20:03:02 +0200

    update

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -40,6 +40,7 @@
 				repeat_rate = 50; # Sets the repeat rate
 				repeat_delay = 300; # Sets the repeat delay
 				follow_mouse = true; # sets focus to follow mouse
+				force_no_accel = true; # Disables Acceleration
 				kb_layout = "us, ro"; # Sets keyboard language and layout
 				touchpad.natural_scroll = true; # Enables natural scroll
 				kb_options = "grp:win_space_toggle"; # Sets the layout switching keybind
