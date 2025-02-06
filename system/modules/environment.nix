0098bf0c5c1a99349d44cb6058a1c45294f331a6
Lukerator <lucadarius41@yahoo.com>
2025-02-06 16:26:50 +0200

    update

--- system/modules/environment.nix
+++ system/modules/environment.nix
@@ -1,9 +1,8 @@
-{ lib, ... }:
 {
 	environment.sessionVariables = {
 		#XCURSOR_SIZE = lib.mkDefault "24"; # Sets XCursor size
 		#HYPRCURSOR_SIZE = "24"; # Sets Hyprcursor size
 		FLAKE = "/home/luke/.nix/"; # Sets the flake location
-		QT_QPA_PLATFORMTHEME = lib.mkForce "qt6ct"; # Sets the Qt theme to qt6ct
+		#QT_QPA_PLATFORMTHEME = lib.mkForce "qt6ct"; # Sets the Qt theme to qt6ct
 	};
 }
