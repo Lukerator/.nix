de2ba820275e01d6e41398b6ff7b647936e2740c
Lukerator <lucadarius41@yahoo.com>
2025-02-11 15:40:30 +0200

    update

--- home/modules/nixvim/plugins/bundle.nix
+++ home/modules/nixvim/plugins/bundle.nix
@@ -2,13 +2,13 @@
 	imports = [
 		./barbar.nix
 		./basic.nix
-		./blink.nix
+		#./blink.nix
 		./conform.nix
 		./dashboard.nix
 		./lsp.nix
 		./lualine.nix
 		./neotree.nix
-		#./nvimcmp.nix
+		./nvimcmp.nix
 		./obsidian.nix
 		./telescope.nix
 		./treesitter.nix
