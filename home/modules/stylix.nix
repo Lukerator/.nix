2b8ba608c0785d4177eed17d42eeb45bfbe5d911
Lukerator <lucadarius41@yahoo.com>
2025-02-17 15:41:18 +0200

    update

--- home/modules/stylix.nix
+++ home/modules/stylix.nix
@@ -4,11 +4,6 @@
 		enable = true; # Enables Stylix
 		polarity = "dark"; # Sets theme to dark
 		cursor.size = 24; # Sets cursor size
-		targets.zathura.enable = true; # Zathura theming
-		targets.waybar = {
-			enable = true;
-			addCss = true;
-		};
 		image = /home/luke/.nix/home/modules/background/background.png; # Sets wallpaper image
 		base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 		#base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml"; # Sets color scheme
--- home/modules/waybar.nix
+++ home/modules/waybar.nix
@@ -1,7 +1,7 @@
 {
 	programs.waybar = {
 		enable = true;
-		#style = builtins.readFile ./waybar/style.css;
+		style = builtins.readFile ./waybar/style.css;
 		settings = [{
 			layer = "top";
 			position = "top";
