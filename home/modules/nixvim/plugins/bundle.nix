fdf7148791158ff98700c1adffbe6a865bbafc37
Lukerator <lucadarius41@yahoo.com>
2025-02-25 23:30:34 +0200

    update

--- home/modules/nixvim/plugins/bundle.nix
+++ home/modules/nixvim/plugins/bundle.nix
@@ -1,15 +1,15 @@
 {
 	imports = [
-		#./barbar.nix
 		./basic.nix
+		./bufferline.nix
 		./cmp.nix
 		./codeium.nix
-		#./conform.nix
 		./dashboard.nix
 		./lsp.nix
 		./lualine.nix
-		#./neotree.nix
 		./telescope.nix
 		./treesitter.nix
+		#./conform.nix
+		#./neotree.nix
 	];
 }
