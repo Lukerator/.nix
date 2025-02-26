c829121da77d5043124f222767439643c0e2aec0
Lukerator <lucadarius41@yahoo.com>
2025-02-26 17:51:27 +0200

    update

--- home/modules/nixvim/plugins/bundle.nix
+++ home/modules/nixvim/plugins/bundle.nix
@@ -3,6 +3,7 @@
 		./basic.nix
 		./cmp.nix
 		./codeium.nix
+		./codesnap.nix
 		./dashboard.nix
 		./lsp.nix
 		./lualine.nix
--- system/modules/environment.nix
+++ system/modules/environment.nix
@@ -2,6 +2,7 @@
 	environment.sessionVariables = {
 		FLAKE = "/home/luke/.nix/"; # Sets the flake location
 		NIXPKGS_ALLOW_UNFREE = 1;
+		HYPRSHOT_DIR = "/home/luke/Pictures/Screenshots/";
 		#XCURSOR_SIZE = lib.mkDefault "24"; # Sets XCursor size
 		#HYPRCURSOR_SIZE = "24"; # Sets Hyprcursor size
 		#QT_QPA_PLATFORMTHEME = lib.mkForce "qt6ct"; # Sets the Qt theme to qt6ct
