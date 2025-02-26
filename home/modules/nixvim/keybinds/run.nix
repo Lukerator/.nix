b08cd97cecb121be6595f2a1f2763a38e8f43551
Lukerator <lucadarius41@yahoo.com>
2025-02-26 20:15:55 +0200

    update

--- home/modules/nixvim/keybinds/run.nix
+++ home/modules/nixvim/keybinds/run.nix
@@ -4,31 +4,31 @@
 			mode = "n";
 			key = "<leader>gc";
 			options.desc = "[G]enerate and run [C]++ file";
-			action = ''<cmd>FloatermNew --cmd="g++ -o out % && ./out && rm ./out<CR>i"'';
+			action = '':FloatermNew clear && g++ -o out % && ./out && rm ./out<CR>i'';
 		}
 		{
 			mode = "n";
 			key = "<leader>gl";
 			options.desc = "[G]o [L]ua file";
-			action = ''<cmd>FloatermNew --cmd="clear && lua %<CR>i"'';
+			action = '':FloatermNew clear && lua %<CR>i'';
 		}
 		{
 			mode = "n";
 			key = "<leader>gp";
 			options.desc = "[G]o [P]ython file";
-			action = ''<cmd>FloatermNew --cmd="clear && python3 %<CR>i"'';
+			action = '':FloatermNew clear && python3 %<CR>i'';
 		}
 		{
 			mode = "n";
 			key = "<leader>gt";
 			options.desc = "[G]o [T]ypst file";
-			action = "<cmd>! typst %<CR><CR>";
+			action = ":! typst %<CR><CR>";
 		}
 		{
 			mode = "n";
 			key = "<leader>gz";
 			options.desc = "[G]o open [Z]athura";
-			action = "<cmd>! zathura %:r.pdf &<CR><CR>";
+			action = ":! zathura %:r.pdf &<CR><CR>";
 		}
 	];
 }
