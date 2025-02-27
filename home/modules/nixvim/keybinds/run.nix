c697cb3cc296b841276b433338048b960b92d516
Lukerator <lucadarius41@yahoo.com>
2025-02-27 21:21:24 +0200

    update

--- home/modules/nixvim/keybinds/run.nix
+++ home/modules/nixvim/keybinds/run.nix
@@ -4,7 +4,7 @@
 			mode = "n";
 			key = "<leader>gc";
 			options.desc = "[G]enerate and run [C]++ file";
-			action = '':FloatermNew --autoclose=0 clear && g++ -o out % && ./out && rm ./out<CR>'';
+			action = '':FloatermNew --autoclose=0 clear && g++ -o %:rnil % && ./%:rnil<CR>'';
 		}
 		{
 			mode = "n";
