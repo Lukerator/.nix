4aac32c845dcf7393d919ab229e0acce5fc46481
Lukerator <lucadarius41@yahoo.com>
2025-02-27 20:58:24 +0200

    update

--- home/modules/nixvim/plugins/debug.nix
+++ home/modules/nixvim/plugins/debug.nix
@@ -1,76 +1,82 @@
+{ pkgs, ... }:
 {
-	programs.nixvim.plugins = {
-		dap-lldb.enable = true;
-		dap-ui = {
-			enable = true;
-			settings = {
-				icons = {
-					expanded = "▾";
-					collapsed = "▸";
-					current_frame = "*";
-				};
-				controls.icons = {
-					play = "▶";
-					pause = "⏸";
-					step_into = "⏎";
-					step_out = "⏮ ";
-					run_last = "▶▶";
-					terminate = "⏹";
-					disconnect = "⏏";
-					step_over = "⏭ ";
-					step_back = "⥢ ";
+	programs.nixvim = {
+		extraPackages = with pkgs; [
+			lldb
+		];
+		plugins = {
+			dap-lldb.enable = true;
+			dap-ui = {
+				enable = true;
+				settings = {
+					icons = {
+						expanded = "▾";
+						collapsed = "▸";
+						current_frame = "*";
+					};
+					controls.icons = {
+						play = "▶";
+						pause = "⏸";
+						step_into = "⏎";
+						step_out = "⏮ ";
+						run_last = "▶▶";
+						terminate = "⏹";
+						disconnect = "⏏";
+						step_over = "⏭ ";
+						step_back = "⥢ ";
+					};
 				};
 			};
-		};
-		dap = {
-			enable = true;
-			configurations = {
-				cpp = [
-					{
-						type = "lldb";
-						name = "Debug";
-						request = "launch";
-						stopOnEntry = false;
-						cwd = "$\${workspaceFolder}";
-						program.__raw = ''
-							function()
-								local cwd = string.format("%s%s", vim.fn.getcwd(), sep)
-								return vim.fn.input("Path to executable: ", cwd, "file")
-							end
-						'';
-					}
-					{
-						type = "lldb";
-						request = "launch";
-						stopOnEntry = false;
-						name = "Debug (+args)";
-						cwd = "$\${workspaceFolder}";
-						program.__raw = ''
-							function()
-								local cwd = string.format("%s%s", vim.fn.getcwd(), sep)
-								return vim.fn.input("Path to executable: ", cwd, "file")
-							end
-						'';
-						args.__raw = ''
-							function()
-								local args = vim.fn.input("Enter arguments: ")
-								return vim.split(args, " ", { trimempty = true })
-							end
-						'';
-					}
-					{
-						type = "lldb";
-						name = "Attach";
-						request = "attach";
-						stopOnEntry = false;
-						program.__raw = ''
-							function()
-								local cwd = string.format("%s%s", vim.fn.getcwd(), sep)
-								return vim.fn.input("Path to executable: ", cwd, "file")
-							end
-						'';
-					}
-				];
+			dap = {
+				enable = true;
+				configurations = {
+					cpp = [
+						{
+							type = "lldb";
+							name = "Debug";
+							request = "launch";
+							stopOnEntry = false;
+							cwd = "$\${workspaceFolder}";
+							program.__raw = ''
+								function()
+									local cwd = string.format("%s%s", vim.fn.getcwd(), sep)
+									return vim.fn.input("Path to executable: ", cwd, "file")
+								end
+							'';
+						}
+						{
+							type = "lldb";
+							request = "launch";
+							stopOnEntry = false;
+							name = "Debug (+args)";
+							cwd = "$\${workspaceFolder}";
+							program.__raw = ''
+								function()
+									local cwd = string.format("%s%s", vim.fn.getcwd(), sep)
+									return vim.fn.input("Path to executable: ", cwd, "file")
+								end
+							'';
+							args.__raw = ''
+								function()
+									local args = vim.fn.input("Enter arguments: ")
+									return vim.split(args, " ", { trimempty = true })
+								end
+							'';
+						}
+						{
+							type = "lldb";
+							name = "Attach";
+							request = "attach";
+							stopOnEntry = false;
+							program.__raw = ''
+								function()
+									local cwd = string.format("%s%s", vim.fn.getcwd(), sep)
+									return vim.fn.input("Path to executable: ", cwd, "file")
+								end
+							'';
+						}
+					];
+				};
 			};
 		};
 	};
--- home/modules/packages.nix
+++ home/modules/packages.nix
@@ -2,7 +2,6 @@
 {
 	nixpkgs.config.allowUnfree = true;
 	home.packages = with pkgs; [
-		lldb
 		oh-my-zsh
 		inputs.zen-browser.packages."${system}".twilight
 	]; # Enables user packages
