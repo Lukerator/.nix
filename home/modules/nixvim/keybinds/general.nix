c921f148d819a12853aeb6c85ddfd5e172476c46
Lukerator <lucadarius41@yahoo.com>
2025-02-28 22:32:36 +0200

    update

--- home/modules/nixvim/keybinds/general.nix
+++ home/modules/nixvim/keybinds/general.nix
@@ -31,19 +31,19 @@
 			mode = [ "n" ];
 			key = "<leader>tt";
 			options.desc = "Toggle Terminal";
-			action.__raw = "<cmd>FloatermNew --cwd=~<CR>";
+			action.__raw = ''<cmd>FloatermNew --cwd=~<CR>'';
 		}
 		{
 			mode = [ "n" ];
 			key = "<leader>th";
 			options.desc = "Toggle Terminal Here";
-			action.__raw = "<cmd>FloatermNew --cwd=%:p:h<CR>";
+			action.__raw = ''<cmd>FloatermNew --cwd=%:p:h<CR>'';
 		}
 		{
 			mode = [ "n" ];
 			key = "<leader>tb";
 			options.desc = "Toggle Terminal Big";
-			action.__raw = "<cmd>FloatermNew --height=0.9 --width=0.9 --cwd=~<CR>";
+			action.__raw = ''<cmd>FloatermNew --height=0.9 --width=0.9 --cwd=~<CR>'';
 		}
 	];
 }
