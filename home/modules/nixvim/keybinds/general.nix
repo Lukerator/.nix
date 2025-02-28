6c1b0c3c9b350566a238a21ebcbb2d5b0f6d779e
Lukerator <lucadarius41@yahoo.com>
2025-02-28 11:46:36 +0200

    update

--- home/modules/nixvim/keybinds/general.nix
+++ home/modules/nixvim/keybinds/general.nix
@@ -18,33 +18,44 @@
 		{
 			key = "<leader>sb";
 			mode = [ "n" "v" ];
-			action.__raw = "function()
+			action.__raw = ''function()
+				for _, item in ipairs(Snacks.scratch.list()) do
+					item._path = item.file
+					item.branch = item.branch and ("branch:%s"):format(item.branch) or ""
+					item.cwd = item.cwd and vim.fn.fnamemodify(item.cwd, ":p:~") or ""
+					item.icon = item.icon or Snacks.util.icon(item.ft, "filetype")
+					item.preview = { text = item.file }
+					{ 0, 0, 0, 0 }[1] = math.max({ 0, 0, 0, 0 }[1], vim.api.nvim_strwidth(item.cwd))
+					{ 0, 0, 0, 0 }[1] = math.max({ 0, 0, 0, 0 }[1], vim.api.nvim_strwidth(item.icon))
+					{ 0, 0, 0, 0 }[1] = math.max({ 0, 0, 0, 0 }[1], vim.api.nvim_strwidth(item.name))
+					{ 0, 0, 0, 0 }[1] = math.max({ 0, 0, 0, 0 }[1], vim.api.nvim_strwidth(item.branch))
+				end
 				Snacks.picker.pick({
-					source = 'scratch',
+					source = "scratch",
 					items = Snacks.scratch.list(),
-					format = 'text',
+					format = "text",
 					layout = {
-						layout = { title = ' Select Scratch Buffer: ' },
+						layout = { title = "Select Scratch Buffer: " },
 						preview = true,
 						preset = function()
-							return vim.o.columns >= 120 and 'default' or 'vertical'
+							return vim.o.columns >= 120 and "default" or "vertical"
 						end,
 					},
 					on_change = function()
 						vim.cmd.startinsert()
 					end,
 					transform = function(item)
-						item.text = format_item_text(item)
+						items.text = format_item_text(Snacks.scratch.list())
 					end,
 					win = {
 						input = {
 							keys = {
-								['<c-x>'] = { 'delete', mode = { 'i', 'n' } },
+								["<C-d>"] = { "delete", mode = { "i", "n" } },
 							},
 						},
 					},
 				})
-			end";
+			end'';
 			options.desc = "Search Scratch Buffer";
 		}
 		{
