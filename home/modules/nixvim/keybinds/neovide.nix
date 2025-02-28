2980ba8c1fd5f127f83606f9ba53eebdb6acf888
Lukerator <lucadarius41@yahoo.com>
2025-02-28 21:13:54 +0200

    update

--- home/modules/nixvim/keybinds/neovide.nix
+++ home/modules/nixvim/keybinds/neovide.nix
@@ -1,34 +1,12 @@
 {
-	programs.nixvim.keymaps = [
-		{
-			key = "<C-S-V>";
-			mode = [ "n" "v" ];
-			options.desc = "Paste";
-			action = ''"+P'';
-		}
-		{
-			key = "<C-S-V>";
-			mode = [ "i" ];
-			options.desc = "Paste";
-			action = ''<C-r>+'';
-		}
-		{
-			key = "<C-S-C>";
-			mode = [ "v" ];
-			options.desc = "Copy";
-			action = ''"+y'';
-		}
-		{
-			key = "<C-+>";
-			mode = [ "n" "v" ];
-			options.desc = "Zoom In";
-			action.__raw = "vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1";
-		}
-		{
-			key = "<C-->";
-			mode = [ "n" "v" ];
-			options.desc = "Zoom Out";
-			action.__raw = "vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1";
-		}
-	];
+	programs.nixvim.extraConfigLuaPost = ''
+		if vim.g.neovide then
+			vim.keymap.set("v", "<C-S-c>", '"+y')
+			vim.keymap.set("i", "<C-S-v>", '<C-R>+')
+			vim.keymap.set({ "n", "v" }, "<C-S-v>", '"+p')
+			vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+			vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+			vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+		end
+	'';
 }
