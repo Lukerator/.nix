ee5c0e46ce6585996825b74180860293ae9bf0a0
Lukerator <lucadarius41@yahoo.com>
2025-02-28 21:54:34 +0200

    update

--- home/modules/nixvim/keybinds/neovide.nix
+++ home/modules/nixvim/keybinds/neovide.nix
@@ -7,7 +7,7 @@
 			vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", {silent = true, noremap = true})
 			vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", {silent = true, noremap = true})
 			vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", {silent = true, noremap = true})
-			vim.o.guifont = { "JetBrainsMono Nerd", ":12" }
+			vim.o.guifont = "JetBrainsMono Nerd:12"
 		end
 	'';
 }
