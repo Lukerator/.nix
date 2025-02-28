24850eb47cea5c284f6799e4e4d2ac72c0ce9b57
Lukerator <lucadarius41@yahoo.com>
2025-02-28 11:50:32 +0200

    update

--- home/modules/nixvim/keybinds/general.nix
+++ home/modules/nixvim/keybinds/general.nix
@@ -18,51 +18,7 @@
 		{
 			key = "<leader>sb";
 			mode = [ "n" "v" ];
-			action.__raw = ''function()
-				for _, item in ipairs(Snacks.scratch.list()) do
-					item._path = item.file
-					item.branch = item.branch and ("branch:%s"):format(item.branch) or ""
-					item.cwd = item.cwd and vim.fn.fnamemodify(item.cwd, ":p:~") or ""
-					item.icon = item.icon or Snacks.util.icon(item.ft, "filetype")
-					item.preview = { text = item.file }
-					{ 0, 0, 0, 0 }[1] = math.max({ 0, 0, 0, 0 }[1], vim.api.nvim_strwidth(item.cwd))
-					{ 0, 0, 0, 0 }[1] = math.max({ 0, 0, 0, 0 }[1], vim.api.nvim_strwidth(item.icon))
-					{ 0, 0, 0, 0 }[1] = math.max({ 0, 0, 0, 0 }[1], vim.api.nvim_strwidth(item.name))
-					{ 0, 0, 0, 0 }[1] = math.max({ 0, 0, 0, 0 }[1], vim.api.nvim_strwidth(item.branch))
-				end
-				local function format_item_text(items)
-					local parts = { item.cwd, item.icon, item.name, item.branch }
-					for i, part in ipairs(parts) do
-						parts[i] = part .. string.rep(" ", column_widths[i] - vim.api.nvim_strwidth(part))
-					end
-					return table.concat(parts, " ")
-				end
-				Snacks.picker.pick({
-					source = "scratch",
-					items = Snacks.scratch.list(),
-					format = "text",
-					layout = {
-						layout = { title = "Select Scratch Buffer: " },
-						preview = true,
-						preset = function()
-							return vim.o.columns >= 120 and "default" or "vertical"
-						end,
-					},
-					on_change = function()
-						vim.cmd.startinsert()
-					end,
-					transform = function(item)
-						items.text = format_item_text(Snacks.scratch.list())
-					end,
-					win = {
-						input = {
-							keys = {
-								["<C-d>"] = { "delete", mode = { "i", "n" } },
-							},
-						},
-					},
-				})
-			end'';
+			action.__raw = "function() Snacks.scratch.select() end";
 			options.desc = "Search Scratch Buffer";
 		}
 		{
