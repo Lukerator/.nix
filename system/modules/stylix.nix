e5363cfa018984f56fadb501aa19b51393c6713e
Lukerator <lucadarius41@yahoo.com>
2025-03-02 21:39:41 +0200

    update

--- system/modules/stylix.nix
+++ system/modules/stylix.nix
@@ -4,7 +4,8 @@
 		enable = true; # Enables Stylix
 		polarity = "dark"; # Sets dark color scheme
 		cursor.size = 24; # Sets cursor size
-		base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+		#base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+		base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
 		#base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml"; # Sets color scheme
 	};
 }
