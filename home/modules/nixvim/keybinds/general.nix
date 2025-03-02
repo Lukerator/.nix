c10288a3fbb655574aff5a9c49941916f3a0fd7f
Lukerator <lucadarius41@yahoo.com>
2025-03-02 20:11:22 +0200

    update

--- home/modules/nixvim/keybinds/general.nix
+++ home/modules/nixvim/keybinds/general.nix
@@ -27,5 +27,29 @@
 			options.desc = "Toggle Scratch Buffer";
 			action.__raw = "function() Snacks.scratch() end";
 		}
+		{
+			mode = [ "n" ];
+			key = "<leader>uh";
+			options.desc = "Update home";
+			action.__raw = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic home-update<cr>";
+		}
+		{
+			mode = [ "n" ];
+			key = "<leader>us";
+			options.desc = "Update system";
+			action.__raw = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic system-update<cr>";
+		}
+		{
+			mode = [ "n" ];
+			key = "<leader>upd";
+			options.desc = "Update everything";
+			action.__raw = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic update<cr>";
+		}
+		{
+			mode = [ "n" ];
+			key = "<leader>upg";
+			options.desc = "Upgrade everything";
+			action.__raw = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic upgrade<cr>";
+		}
 	];
 }
