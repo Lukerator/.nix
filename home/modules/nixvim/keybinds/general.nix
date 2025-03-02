29d45aa1de7b519ff914028c3cf341e65e7d8e58
Lukerator <lucadarius41@yahoo.com>
2025-03-02 22:05:59 +0200

    update

--- home/modules/nixvim/keybinds/general.nix
+++ home/modules/nixvim/keybinds/general.nix
@@ -16,6 +16,11 @@
 			action.__raw = "vim.diagnostic.open_float";
 		}
 		{
+			mode = "n";
+			key = "<leader>mk";
+			action = "<cmd>Markview Toggle<CR>>";
+		}
+		{
 			key = "<leader>sb";
 			mode = [ "n" "v" ];
 			action.__raw = "function() Snacks.scratch.select() end";
@@ -31,25 +36,25 @@
 			mode = [ "n" ];
 			key = "<leader>uh";
 			options.desc = "Update home";
-			action = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic home-update<cr>";
+			action = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic home-update<CR>>";
 		}
 		{
 			mode = [ "n" ];
 			key = "<leader>us";
 			options.desc = "Update system";
-			action = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic system-update<cr>";
+			action = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic system-update<CR>>";
 		}
 		{
 			mode = [ "n" ];
 			key = "<leader>upd";
 			options.desc = "Update everything";
-			action = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic update<cr>";
+			action = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic update<CR>>";
 		}
 		{
 			mode = [ "n" ];
 			key = "<leader>upg";
 			options.desc = "Upgrade everything";
-			action = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic upgrade<cr>";
+			action = "<cmd>FloatermNew --height=0.8 --width=0.8 --cwd=~ zsh -ic upgrade<CR>>";
 		}
 	];
 }
