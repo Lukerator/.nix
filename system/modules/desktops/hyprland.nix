23f93f59187dace8f81161f508f6de144c56d97a
Lukerator <lucadarius41@yahoo.com>
2025-03-03 18:15:12 +0200

    update

--- system/modules/desktops/hyprland.nix
+++ system/modules/desktops/hyprland.nix
@@ -1,20 +1,17 @@
 { pkgs, inputs, ... }: {
+	environment = {
+		sessionVariables.NIXOS_OZONE_WL = "1"; # Enables Wayland for apps that semi-support it
+	};
+	nix.settings = {
+		substituters = ["https://hyprland.cachix.org"];
+		trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+	};
 	programs.hyprland = {
 		enable = true; # Enables Hyprland as a Window Manager/Desktop Environment
 		withUWSM = false;
 		package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
 		portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-
 	};
-	/*services = {
-		xserver.enable = true;
-		displayManager.sddm = {
-			enable = true; # Enables SDDM - see ./plasma.nix
-			#wayland.enable = true; # Enables Wayland on SDDM
-			theme = "catppuccin-mocha"; # Sets SDDM theme
-			package = pkgs.kdePackages.sddm; # Enables Qt6
-		};
-	};*/
 	services = {
 		libinput = {
 			enable = true;
@@ -28,11 +25,13 @@
 			};
 		};
 	};
-	environment = {
-		sessionVariables.NIXOS_OZONE_WL = "1"; # Enables Wayland for apps that semi-support it
-	};
-	nix.settings = {
-		substituters = ["https://hyprland.cachix.org"];
-		trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-	};
+	/* services = {
+		xserver.enable = true;
+		displayManager.sddm = {
+			enable = true; # Enables SDDM - see ./plasma.nix
+			#wayland.enable = true; # Enables Wayland on SDDM
+			theme = "catppuccin-mocha"; # Sets SDDM theme
+			package = pkgs.kdePackages.sddm; # Enables Qt6
+		};
+	}; */
 }
