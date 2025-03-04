1dfd45c020a922c5802e76c5a3746163f80ea492
Lukerator <lucadarius41@yahoo.com>
2025-03-04 16:59:19 +0200

    update

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
 	programs.zsh = {
 		enable = true; # Enables ZShell
@@ -7,10 +8,26 @@
 		oh-my-zsh = {
 			enable = true; # Enables oh-my-zsh
 			theme = "robbyrussell"; # Sets oh-my-zsh theme
-			custom = "./zsh/";
 			plugins = [
-				"nix-shell"
-				"nix-zsh-completions"
+				{
+					name = "zsh-nix-shell";
+					file = "nix-shell.plugin.zsh";
+					src = pkgs.fetchFromGitHub {
+						owner = "chisui";
+						repo = "zsh-nix-shell";
+						rev = "v0.8.0";
+						sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+					};
+				}
+				{
+					name = "nix-zsh-completions";
+					file = "nix-zsh-completions.plugin.zsh";
+					src = pkgs.fetchFromGitHub {
+						owner = "nix-community";
+						repo = "nix-zsh-completions";
+						rev = "v0.5.1";
+					};
+				}
 			];
 		};
 		shellAliases = {
--- system/environment.nix
+++ system/environment.nix
@@ -3,7 +3,6 @@
 		FLAKE = "/home/luke/.nix/"; # Sets the flake location
 		NIXPKGS_ALLOW_UNFREE = 1;
 		HYPRSHOT_DIR = "/home/luke/Pictures/Screenshots/";
-		ZSH_CUSTOM = "./.oh-my-zsh/";
 		#XCURSOR_SIZE = lib.mkDefault "24"; # Sets XCursor size
 		#HYPRCURSOR_SIZE = "24"; # Sets Hyprcursor size
 		#QT_QPA_PLATFORMTHEME = lib.mkForce "qt6ct"; # Sets the Qt theme to qt6ct
