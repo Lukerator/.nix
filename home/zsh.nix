4fd6dd1ab4281eda40119589af647d1267bfe37c
Lukerator <lucadarius41@yahoo.com>
2025-03-04 17:10:37 +0200

    update

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,4 +1,3 @@
-{ pkgs, ... }:
 {
 	programs.zsh = {
 		enable = true; # Enables ZShell
@@ -22,26 +21,5 @@
 			home-update = "git-update && home-update-no-git";
 			system-update = "git-update && system-update-no-git";
 		}; # Sets aliases
-		plugins = [
-			{
-				name = "zsh-nix-shell";
-				file = "nix-shell.plugin.zsh";
-				src = pkgs.fetchFromGitHub {
-					owner = "chisui";
-					repo = "zsh-nix-shell";
-					rev = "v0.8.0";
-					sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-				};
-			}
-			{
-				name = "nix-zsh-completions";
-				file = "nix-zsh-completions.plugin.zsh";
-				src = pkgs.fetchFromGitHub {
-					owner = "nix-community";
-					repo = "nix-zsh-completions";
-					rev = "v0.5.1";
-				};
-			}
-		];
 	};
 }
