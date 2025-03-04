991cd56b8f2932f9122d934fea8fef437215d746
Lukerator <lucadarius41@yahoo.com>
2025-03-04 17:03:43 +0200

    update

--- home/zsh.nix
+++ home/zsh.nix
@@ -8,27 +8,6 @@
 		oh-my-zsh = {
 			enable = true; # Enables oh-my-zsh
 			theme = "robbyrussell"; # Sets oh-my-zsh theme
-			plugins = [
-				{
-					name = "zsh-nix-shell";
-					file = "nix-shell.plugin.zsh";
-					src = pkgs.fetchFromGitHub {
-						owner = "chisui";
-						repo = "zsh-nix-shell";
-						rev = "v0.8.0";
-						sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-					};
-				}
-				{
-					name = "nix-zsh-completions";
-					file = "nix-zsh-completions.plugin.zsh";
-					src = pkgs.fetchFromGitHub {
-						owner = "nix-community";
-						repo = "nix-zsh-completions";
-						rev = "v0.5.1";
-					};
-				}
-			];
 		};
 		shellAliases = {
 			ls = "lsd";
@@ -43,5 +22,26 @@
 			home-update = "git-update && home-update-no-git";
 			system-update = "git-update && system-update-no-git";
 		}; # Sets aliases
+		plugins = [
+			{
+				name = "zsh-nix-shell";
+				file = "nix-shell.plugin.zsh";
+				src = pkgs.fetchFromGitHub {
+					owner = "chisui";
+					repo = "zsh-nix-shell";
+					rev = "v0.8.0";
+					sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+				};
+			}
+			{
+				name = "nix-zsh-completions";
+				file = "nix-zsh-completions.plugin.zsh";
+				src = pkgs.fetchFromGitHub {
+					owner = "nix-community";
+					repo = "nix-zsh-completions";
+					rev = "v0.5.1";
+				};
+			}
+		];
 	};
 }
