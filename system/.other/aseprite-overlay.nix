c069cc6208d36e5a130ea56d67f62f9716e3b651
Lukerator <lucadarius41@yahoo.com>
2025-03-11 22:31:06 +0200

    update

--- system/.other/aseprite-overlay.nix
+++ system/.other/aseprite-overlay.nix
@@ -6,19 +6,15 @@ self: super: {
 		cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DENABLE_SKIA=ON" ];
 		nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ super.ninja ];
 		src = super.fetchgit {
+			fetchSubmodules = true;
 			url = "https://github.com/aseprite/aseprite.git";
-			# hash = "sha256-+M2idJs5nNvFYNYQe9nCU/+WMF4Y7E8rBw/3F38BWHY=";
 			hash = "sha256-cwlByoLMD90ex97n5kdSM7FOdKK1bvMDXA1AJdaGDV0=";
 		};
 		/* src = super.fetchFromGitHub {
 			rev = "";
 			repo = "aseprite";
 			owner = "aseprite";
-			# rev = "v1.3.14-beta1";
-			# sha256 = "pJ4YljF9wut9SYW3v+gcOswv8nRSzvjIfbmG0vjBDqQ";
-			# hash = "sha256-pJ4YljF9wut9SYW3v+gcOswv8nRSzvjIfbmG0vjBDqQ=";
-			hash = "sha256-+M2idJs5nNvFYNYQe9nCU/+WMF4Y7E8rBw/3F38BWHY=";
-			# sha256 = "1jqhhx5ca7scgc8p8n47zykg0chk7hhpsmh5d2fxi5clhpc22jz0";
+			hash = "sha256-cwlByoLMD90ex97n5kdSM7FOdKK1bvMDXA1AJdaGDV0=";
 		}; */
 	});
 }
