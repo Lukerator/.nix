b03f795c9130bb2ac96703151e0f92187e5309f4
Lukerator <lucadarius41@yahoo.com>
2025-03-11 22:22:50 +0200

    update

--- system/.other/aseprite-overlay.nix
+++ system/.other/aseprite-overlay.nix
@@ -3,7 +3,13 @@ self: super: {
 		patches = [];
 		postPatch = "";
 		version = "1.3.14-beta1";
-		src = super.fetchFromGitHub {
+		cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DENABLE_SKIA=ON" ];
+		nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ super.ninja ];
+		src = super.fetchGit {
+			url = "https://github.com/aseprite/aseprite.git";
+			hash = "sha256-+M2idJs5nNvFYNYQe9nCU/+WMF4Y7E8rBw/3F38BWHY=";
+		};
+		/* src = super.fetchFromGitHub {
 			rev = "";
 			repo = "aseprite";
 			owner = "aseprite";
@@ -12,8 +18,6 @@ self: super: {
 			# hash = "sha256-pJ4YljF9wut9SYW3v+gcOswv8nRSzvjIfbmG0vjBDqQ=";
 			hash = "sha256-+M2idJs5nNvFYNYQe9nCU/+WMF4Y7E8rBw/3F38BWHY=";
 			# sha256 = "1jqhhx5ca7scgc8p8n47zykg0chk7hhpsmh5d2fxi5clhpc22jz0";
-		};
-		nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ super.ninja ];
-		cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DENABLE_SKIA=ON" ];
+		}; */
 	});
 }
