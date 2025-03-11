25ec6feac2faf54deef67337b31809e775c289d2
Lukerator <lucadarius41@yahoo.com>
2025-03-11 22:12:32 +0200

    update

--- system/.other/aseprite-overlay.nix
+++ system/.other/aseprite-overlay.nix
@@ -7,6 +7,7 @@ self: super: {
 			rev = "v1.3.14-beta1";
 			# sha256 = "pJ4YljF9wut9SYW3v+gcOswv8nRSzvjIfbmG0vjBDqQ";
 			# hash = "sha256-pJ4YljF9wut9SYW3v+gcOswv8nRSzvjIfbmG0vjBDqQ=";
+			hash = "sha256-+M2idJs5nNvFYNYQe9nCU/+WMF4Y7E8rBw/3F38BWHY=";
 			# sha256 = "1jqhhx5ca7scgc8p8n47zykg0chk7hhpsmh5d2fxi5clhpc22jz0";
 		};
 		nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ super.ninja ];
