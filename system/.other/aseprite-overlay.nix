16056b46fff30b8af287db330f389dacd584262d
Lukerator <lucadarius41@yahoo.com>
2025-03-11 22:34:53 +0200

    update

--- system/.other/aseprite-overlay.nix
+++ system/.other/aseprite-overlay.nix
@@ -4,12 +4,23 @@ self: super: {
 		postPatch = "";
 		version = "1.3.14-beta1";
 		cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DENABLE_SKIA=ON" ];
-		nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ super.ninja ];
+		nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+			# super.cmake
+			super.ninja
+			# super.pkg-config
+		];
 		src = super.fetchgit {
 			fetchSubmodules = true;
 			url = "https://github.com/aseprite/aseprite.git";
 			hash = "sha256-cwlByoLMD90ex97n5kdSM7FOdKK1bvMDXA1AJdaGDV0=";
 		};
+		buildInputs = oldAttrs.buildInputs ++ [
+			# super.glfw
+			# super.skia
+			# super.libpng
+			# super.freetype
+			super.tinyxml2
+		];
 		/* src = super.fetchFromGitHub {
 			rev = "";
 			repo = "aseprite";
