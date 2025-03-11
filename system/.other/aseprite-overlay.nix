01729ab577eac9365b9fda0e291a5a944861b588
Lukerator <lucadarius41@yahoo.com>
2025-03-11 22:08:49 +0200

    update

--- system/.other/aseprite-overlay.nix
+++ system/.other/aseprite-overlay.nix
@@ -6,8 +6,8 @@ self: super: {
 			owner = "aseprite";
 			rev = "v1.3.14-beta1";
 			# sha256 = "pJ4YljF9wut9SYW3v+gcOswv8nRSzvjIfbmG0vjBDqQ";
+			# hash = "sha256-pJ4YljF9wut9SYW3v+gcOswv8nRSzvjIfbmG0vjBDqQ=";
 			# sha256 = "1jqhhx5ca7scgc8p8n47zykg0chk7hhpsmh5d2fxi5clhpc22jz0";
-			hash = "sha256-pJ4YljF9wut9SYW3v+gcOswv8nRSzvjIfbmG0vjBDqQ=";
 		};
 		nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ super.ninja ];
 		cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DENABLE_SKIA=ON" ];
