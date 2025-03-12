883a3594d7c2f9dcd1b4cdc4b53085c86caa39b7
Lukerator <lucadarius41@yahoo.com>
2025-03-12 15:29:38 +0200

    update

--- system/.other/asepritepkg.nix
+++ system/.other/asepritepkg.nix
@@ -1,24 +1,22 @@
-{ lib, pkgs ? import <nixpkgs> { } }:
-pkgs.stdenv.mkDerivation {
+{ lib, stdenv, fetchgit, cmake, ninja, tinyxml2 }:
+stdenv.mkDerivation {
 	pname = "aseprite-beta";
 	version = "1.3.14-beta1";
 	cmakeFlags = [
-		"-DTINYXML2_DIR=${pkgs.tinyxml2}"
-		"-DCMAKE_PREFIX_PATH=${pkgs.tinyxml2}"
+		"-DTINYXML2_DIR=${tinyxml2}"
+		"-DCMAKE_PREFIX_PATH=${tinyxml2}"
 	];
-	src = pkgs.fetchgit {
+	src = fetchgit {
 		rev = "HEAD";
 		url = "https://github.com/aseprite/aseprite.git";
 		hash = "sha256-cwlByoLMD90ex97n5kdSM7FOdKK1bvMDXA1AJdaGDV0=";
 	};
 	nativeBuildInputs = [
-		pkgs.cmake
-		pkgs.ninja
-		pkgs.tinyxml2
+		cmake
+		ninja
 	];
 	buildInputs = [
-		pkgs.skia-aseprite
-		pkgs.sdl3
+		tinyxml2
 	];
 	/* installPhase = ''
 		mkdir -p $out/bin
@@ -26,7 +24,7 @@ pkgs.stdenv.mkDerivation {
 	''; */
 	meta = {
 		description = "Sprite editor";
-		platforms = pkgs.lib.platforms.linux;
+		platforms = lib.platforms.linux;
 		license = lib.licenses.unfree;
 	};
 }
