1b0958eb37a1169b86e8d30f7647f98a45af5caa
Lukerator <lucadarius41@yahoo.com>
2025-03-12 23:48:19 +0200

    update

--- system/aseprite.nix
+++ system/aseprite.nix
@@ -1,55 +1,45 @@
-{ pkgs, fetchFromGitHub, cmake, ninja, python3, clang, libcxx, libcxxabi, ... }:
-
+{ pkgs, ... }:
 let
-  skia = pkgs.stdenv.mkDerivation rec {
-    pname = "skia-aseprite";
-    version = "m117";
-
-    src = fetchFromGitHub {
-      owner = "aseprite";
-      repo = "skia";
-      rev = "aseprite-m117";
-      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    };
-
-    nativeBuildInputs = [ cmake ninja python3 ];
-    buildInputs = [ clang libcxx libcxxabi ];
-
-    buildPhase = ''
-      bin/gn gen out/Release --args="is_official_build=true skia_use_system_libpng=false"
-      ninja -C out/Release
-    '';
-
-    installPhase = ''
-      mkdir -p $out
-      cp -r out/Release $out/
-    '';
-  };
-
-in pkgs.stdenv.mkDerivation rec {
-  pname = "aseprite";
-  version = "1.3.14-beta1";
-
-  src = fetchFromGitHub {
-    owner = "aseprite";
-    repo = "aseprite";
-    rev = "main";
-    sha256 = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
-  };
-
-  nativeBuildInputs = [ cmake ninja pkgs.pkg-config ];
-  buildInputs = [ skia pkgs.boost pkgs.libpng pkgs.zlib pkgs.glfw pkgs.gcc ];
-
-  cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" "-DLAF_BACKEND=skia" ];
-
-  buildPhase = ''
-    mkdir -p build && cd build
-    cmake .. $cmakeFlags
-    ninja aseprite
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp bin/aseprite $out/bin/
-  '';
+	skia = pkgs.stdenv.mkDerivation rec {
+		pname = "skia-aseprite";
+		version = "m124-eadfe707ca";
+		src = pkgs.fetchFromGitHub {
+			owner = "aseprite";
+			repo = "skia";
+			rev = "${version}";
+			sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+		};
+		nativeBuildInputs = with pkgs; [ cmake ninja python3 ];
+		buildInputs = with pkgs; [ clang libcxx libcxxabi ];
+		buildPhase = ''
+			bin/gn gen out/Release --args="is_official_build=true skia_use_system_libpng=false"
+			ninja -C out/Release
+		'';
+		installPhase = ''
+			mkdir -p $out
+			cp -r out/Release $out/
+		'';
+	};
+in
+	pkgs.stdenv.mkDerivation rec {
+		pname = "aseprite";
+		version = "1.3.14-beta1";
+		src = pkgs.fetchFromGitHub {
+			owner = "aseprite";
+			repo = "aseprite";
+			rev = "${version}";
+			sha256 = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
+		};
+		nativeBuildInputs = with pkgs; [ cmake ninja pkgs.pkg-config ];
+		buildInputs = [ skia pkgs.boost pkgs.libpng pkgs.zlib pkgs.glfw pkgs.gcc ];
+		cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" "-DLAF_BACKEND=skia" ];
+		buildPhase = ''
+			mkdir -p build && cd build
+			cmake .. $cmakeFlags
+			ninja aseprite
+		'';
+		installPhase = ''
+			mkdir -p $out/bin
+			cp bin/aseprite $out/bin/
+		'';
 }
--- system/syspackages.nix
+++ system/syspackages.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
 	environment.systemPackages = with pkgs; [
-		(import ./aseprite.nix { inherit pkgs lib stdenv fetchFromGitHub cmake ninja python3 clang libcxx libcxxabi; })
+		(import ./aseprite.nix { inherit pkgs; })
 		brightnessctl
 		calibre
 		dtrx
